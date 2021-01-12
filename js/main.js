2547f20407ccdec29632a00de17fc00f8d064d3a
Olegopolonets <opolonetso@gmail.com>
2021-01-12 20:12:36 +0200

    Сreated the home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+$(function () {
+  $("[data-fancybox]").fancybox({
+    youtube: {
+      controls: 1,
+      showinfo: 0,
+    },
+  });
 
+  $(".heroes__slider-img").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".heroes__slider-text",
+    prevArrow: '<button class="slick-prev slick-btn"><img src="images/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button class="slick-next slick-btn"><img src="images/arrow-right.png" alt="next"></button>',
+  });
+  $(".heroes__slider-text").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: ".heroes__slider-img",
+    arrows: false,
+  });
 });
