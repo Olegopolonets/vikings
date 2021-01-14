fc7b3f131f0ef032d213abd8747732755e3d488e
Olegopolonets <opolonetso@gmail.com>
2021-01-14 20:04:53 +0200

    FULL Responsive site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+  $(".menu__btn").on("click", function () {
+    $(".menu_list").toggleClass("menu__list--active");
+  });
+
   $("[data-fancybox]").fancybox({
     youtube: {
       controls: 1,
@@ -12,6 +16,14 @@ $(function () {
     asNavFor: ".heroes__slider-text",
     prevArrow: '<button class="slick-prev slick-btn"><img src="images/arrow-left.png" alt="prev"></button>',
     nextArrow: '<button class="slick-next slick-btn"><img src="images/arrow-right.png" alt="next"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
   $(".heroes__slider-text").slick({
     slidesToShow: 1,
@@ -20,4 +32,5 @@ $(function () {
     asNavFor: ".heroes__slider-img",
     arrows: false,
   });
+  console.log("11");
 });
